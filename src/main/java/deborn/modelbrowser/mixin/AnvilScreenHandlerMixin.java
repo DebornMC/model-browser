ce06257018a80ca45005dbd2eb85b9c3b0520998
DebornMC <66801882+DebornMC@users.noreply.github.com>
2025-11-26 13:31:59 +0100

    Add reset logic

--- src/main/java/deborn/modelbrowser/mixin/AnvilScreenHandlerMixin.java
+++ src/main/java/deborn/modelbrowser/mixin/AnvilScreenHandlerMixin.java
@@ -1,5 +1,7 @@
 package deborn.modelbrowser.mixin;
 
+import java.util.Optional;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -7,23 +9,33 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 
+import net.minecraft.component.Component;
+import net.minecraft.component.ComponentType;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.EquippableComponent;
+import net.minecraft.component.type.NbtComponent;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtByte;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtInt;
+import net.minecraft.nbt.NbtString;
 import net.minecraft.screen.AnvilScreenHandler;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.StringHelper;
 
 @Mixin(AnvilScreenHandler.class)
 public abstract class AnvilScreenHandlerMixin {
@@ -70,7 +82,7 @@ public abstract class AnvilScreenHandlerMixin {
     private void afterUpdateResult(CallbackInfo ci) {
         AnvilScreenHandler self = (AnvilScreenHandler)(Object)this;
         Identifier id = this.pendingModelId;
-        if (id == null) return;
+        if (id == null) return;                                                                                                                                                                              
 
         Slot output = self.getSlot(2);
         if (!output.hasStack()) return;
@@ -93,6 +105,43 @@ public abstract class AnvilScreenHandlerMixin {
 
         // remove glint
         out.set(DataComponentTypes.ENCHANTMENT_GLINT_OVERRIDE, false);
+        
+        // custom data
+        NbtCompound compound = new NbtCompound();
+        compound.putBoolean("model_browser_data", true);
+        NbtComponent customData = NbtComponent.of(compound);
+        out.set(DataComponentTypes.CUSTOM_DATA, customData);
+
+
         output.setStack(out.copy());
+    }
+
+    @Inject(method = "updateResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/StringHelper;isBlank(Ljava/lang/String;)Z"))
+    private void resetItemModel(
+        CallbackInfo ci,
+        @Local(ordinal = 0) LocalIntRef i, 
+        @Local(ordinal = 1) LocalIntRef j, 
+        @Local(ordinal = 0) LocalRef<ItemStack> itemStack, 
+        @Local(ordinal = 1) LocalRef<ItemStack> itemStack2
+    ) {
+        if(!itemStack.get().contains(DataComponentTypes.CUSTOM_DATA)) return;
+        NbtCompound tag = itemStack.get().get(DataComponentTypes.CUSTOM_DATA).copyNbt();
+        if(!tag.getBoolean("model_browser", false)) {
+            if (StringHelper.isBlank(this.newItemName) || this.newItemName == null) {
+                if (!itemStack.get().contains(DataComponentTypes.CUSTOM_NAME)) {
+                    j.set(1);
+                    i.set(j.get()+i.get());
+                }
+                tag.remove("model_browser_data");
+                if (tag.isEmpty()) {
+                    itemStack2.get().remove(DataComponentTypes.CUSTOM_DATA);
+                } else {
+                    itemStack2.get().set(DataComponentTypes.CUSTOM_DATA, NbtComponent.of(tag));
+                }
+                itemStack2.get().set(DataComponentTypes.ITEM_MODEL, itemStack2.get().getItem().getDefaultStack().get(DataComponentTypes.ITEM_MODEL));
+                itemStack2.get().set(DataComponentTypes.EQUIPPABLE, itemStack2.get().getItem().getDefaultStack().get(DataComponentTypes.EQUIPPABLE));
+                itemStack2.get().remove(DataComponentTypes.ENCHANTMENT_GLINT_OVERRIDE);
+            }
+        }
     }
 }
